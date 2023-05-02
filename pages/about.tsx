80c79c425d5972b557bc102025a6ad2416ff42fc
Thinh La <thinhlq@smartosc.com>
2023-05-02 18:56:15 +0700

    Update SEO Title

---
+++ pages/about.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function About() {
+    return (
+        <main>
+        <h1>Title H1 Demo</h1>
+        <Link href="https://facebook.com/lathinh" target="_blank">Facebook</Link>
+        </main>
+    )
+}
